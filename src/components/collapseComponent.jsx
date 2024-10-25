55630b8b5fe11f18dac7a1a9fc0d96ad0e9a30e1
Cassandre Puisset <cpuisset98@gmail.com>
2024-10-25 15:58:30 +0200

    animation collapseComponent

--- src/components/collapseComponent.jsx
+++ src/components/collapseComponent.jsx
@@ -3,25 +3,35 @@ import vector_down from "../assets/vector_down.png";
 import { useState } from "react";
 
 export default function Collapse({ title, content }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const display = () => { setIsOpen(!isOpen) };
-    return (
-        <div className="collapse-component">
-            {/* va falloir remanier l'agencement des div pour que la sélection concerne toute la barre */}
-            <div className="collapse-component__Title" onClick={display}> 
-                <h2>
-                    {title}
+  const [isOpen, setIsOpen] = useState(false);
+  const display = () => {
+    setIsOpen(!isOpen);
+  };
 
-                </h2>
-                    {isOpen ? (
-                        <img src={vector_down} alt="Replier" className="collapse-component__Title__Icon" />
-                    ) : (
-                        <img src={vector_up} alt="Déplier" className="collapse-component__Title__Icon" />
-                    )}
-            </div>
-            <div>
-                {isOpen && <p className="collapse-component__Content" >{content}</p>}
-            </div>
-        </div>
-    )
+  return (
+    <div className="collapse-component">
+      {/* va falloir remanier l'agencement des div pour que la sélection concerne toute la barre */}
+      <div className="collapse-component__Title" onClick={display}>
+        <h2>{title}</h2>
+        <img
+          src={vector_up}
+          alt="Déplier"
+          className={
+            isOpen
+              ? "collapse-component__Title__Icon down"
+              : "collapse-component__Title__Icon"
+          }
+        />
+      </div>
+      <div
+        className={
+          isOpen
+            ? "collapse-component__Content dropdown"
+            : "collapse-component__Content"
+        }
+      >
+        <p>{content}</p>
+      </div>
+    </div>
+  );
 }
