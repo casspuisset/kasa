b2e8eb146eb420804730e6b7027ac17cfab7c42f
Cassandre Puisset <cpuisset98@gmail.com>
2024-11-01 18:35:04 +0100

    corrections layout media queries

--- src/components/homePageComponent.jsx
+++ src/components/homePageComponent.jsx
@@ -12,17 +12,24 @@ export default function HomePageComponent() {
     setItems(showItems < back.length ? showItems + 6 : showItems);
   };
 
-  console.log(back.length)
+  console.log(back.length);
   return (
     <div class="home-component">
       <div>
-        <Banner image={ImageToTake} texte='Chez vous, partout et ailleurs' page='Home' />
+        <Banner
+          image={ImageToTake}
+          texte="Chez vous, partout et ailleurs"
+          page="Home"
+        />
       </div>
       <div className="home-component__Main">
         <div className="home-component__Main__Gallery">
           {back.slice(0, showItems).map((article) => {
             return (
-              <Link to={`/logement/${article.id}`} className="home-component__Main__Gallery__Card">
+              <Link
+                to={`/logement/${article.id}`}
+                className="home-component__Main__Gallery__Card"
+              >
                 <article key={article.id}>
                   <Card title={article.title} cover={article.cover} />
                   <p>{article.title}</p>
@@ -31,12 +38,12 @@ export default function HomePageComponent() {
             );
           })}
         </div>
-        {showItems < back.length &&
+        {showItems < back.length && (
           <button className="home-component__Main__Button" onClick={showMore}>
             Plus de logements
           </button>
-        }
+        )}
       </div>
     </div>
-  )
+  );
 }
